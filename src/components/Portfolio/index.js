1ca67c75e73b4f51b47e850e6fd2f1a86e780abb
Donniecia  Corke <donnieciamc@gmail.com>
2022-11-16 23:49:56 -0500

    had to delete my whole documents and re-add them to make file start

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+
+import MyMeds from "../../assets/images/my-meds.gif";
+import RunBuddy from "../../assets/images/run-budy.png";
+
+
+function Portfolio() {
+  return (
+    <section class="portfolio">
+
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 g-4">
+        <div class="col">
+          <div class="card">
+            <img
+              src={MyMeds}
+              alt="Mymeds"
+              class="card-img-top"
+            />
+            <div class="card-body">
+
+              <a href="https://github.com/dnecia" target="_blank" rel="noreferrer">
+                <button>GitHub</button>
+              </a>
+              <a href="https://github.com/dnecia" target="_blank" rel="noreferrer">
+                <button>Website</button>
+              </a>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col">
+          <div class="card">
+            <img
+              src={RunBuddy}
+              alt="RunBuddy"
+              class="card-img-top"
+            />
+            <div class="card-body">
+              <a href="https://github.com/dnecia" target="_blank" rel="noreferrer">
+                <button>GitHub</button>
+              </a>
+              <a href="https://github.com/dnecia" target="_blank" rel="noreferrer">
+                <button>Website</button>
+              </a>
+            </div>
+          </div>
+        </div>
+
+      
+
+
+      </div>
+    </section>
+
+
+  );
+}
+
+export default Portfolio;
