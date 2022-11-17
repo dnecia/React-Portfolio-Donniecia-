bf096cf17c99d9880199a4c8f358b7fdb49ad7bf
Donniecia  Corke <donnieciamc@gmail.com>
2022-11-16 22:55:14 -0500

    adding in the footer and using react icons to link socials

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <div className="footer-div">
+      <footer>
+        <ul>
+          <a href="https://github.com/dnecia" target="blank">
+            <li>
+              <FaGithub />
+            </li>
+          </a>
+          <a href="https://www.linkedin.com" target="blank">
+            <li>
+              <FaLinkedin />
+            </li>
+          </a>
+         
+        </ul>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
