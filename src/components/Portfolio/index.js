5b88f466380a0fca5561caf317090ec7b3511b78
Donniecia  Corke <donnieciamc@gmail.com>
2022-11-17 19:48:51 -0500

    adding in project3

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-
+import ResReview from "../../assets/images/res-reviews.png"
 import MyMeds from "../../assets/images/my-meds.gif";
 import RunBuddy from "../../assets/images/run-budy.png";
 
@@ -22,7 +22,25 @@ function Portfolio() {
               <a href="https://github.com/dnecia" target="_blank" rel="noreferrer">
                 <button>GitHub</button>
               </a>
-              <a href="https://github.com/dnecia" target="_blank" rel="noreferrer">
+              <a href="https://mymedlist.herokuapp.com/" target="_blank" rel="noreferrer">
+                <button>Website</button>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card">
+            <img
+              src={ResReview}
+              alt="RestaurantReviews"
+              class="card-img-top"
+            />
+            <div class="card-body">
+              <a href="https://github.com/dnecia/Restaraunt-Reviews/tree/dee" target="_blank" rel="noreferrer">
+                <button>GitHub</button>
+              </a>
+              <a href="https://shrouded-taiga-00254.herokuapp.com/" target="_blank" rel="noreferrer">
                 <button>Website</button>
               </a>
             </div>
